40407b4a7068a874c7adc4065461c1406aeee457
Andres Ramirez <andresramirezz@hotmail.com>
2020-05-10 23:31:52 -0400

    First commit
    Signed-off-by: Andres Ramirez <andresramirezz@hotmail.com>

---
+++ src/com/thinkingInJava/InitializationAndCleanup/Leaf.java
@@ -0,0 +1,13 @@
+package com.thinkingInJava.InitializationAndCleanup;
+
+/**
+ * @author andresr
+ *
+ */
+public class Leaf {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author andresr
+ *
+ */
+module ThinkingInJava {
+}
